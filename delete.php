eb4e0a14281727bb29c1d924612ea7cbc253f04f
Satopp12 <iam3104pr@gmail.com>
2019-10-15 20:04:43 +0900

    テストだよ〜〜

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+// 関数ファイルの読み込み
+include('function.php');
+$pdo = connectToDb();
+
+//1. GETデータ取得
+$id   = $_GET['id'];
+
+//2. DB接続します(エラー処理追加)
+
+
+//3．データ登録SQL作成
+$sql = '';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute();
+
+//4．データ登録処理後
+if ($status == false) {
+  showSqlErrorMsg($stmt);
+} else {
+  //select.phpへリダイレクト
+  header();
+  exit;
+}
